835e7c1cd7b86be7cb8d2ee8b52ee9ba238445d6
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2018-10-26 18:52:41 +0200

    Absolute base for auto scene generation

---
+++ Script/Utilities/SaveScene.gd
@@ -0,0 +1,5 @@
+
+	# https://godotengine.org/qa/903/how-to-save-a-scene-at-run-time
+#var packed_scene = PackedScene.new()
+#packed_scene.pack(get_tree().get_current_scene())
+#ResourceSaver.save("res://my_scene.tscn", packed_scene)
