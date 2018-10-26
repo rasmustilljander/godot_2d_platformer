de12181d94b909a9c39fc8bbb8ccc038ecc5f11c
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2018-10-26 18:52:15 +0200

    Renamed scene and fixed camera to only follow in Y

--- Script/MainCamera.gd
+++ Script/MainCamera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 var player
+export var threshold = 10
 
 func _ready():
 	player = get_node("../Player")
@@ -8,5 +9,6 @@ func _ready():
 
 func _process(delta):
 	var direction = (player.get_position() - get_position())
-	set_position(position + direction * delta)
-	
+	direction.x = 0
+	if(direction.length() > threshold):
+		set_position(position + direction * delta)
